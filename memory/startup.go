8878c95e03bc6a46d92bc6645c7edf471a775abe
fhancic <fhancic@croz.net>
2020-09-26 22:32:32 +0200

    style, refactoring

--- memory/startup.go
+++ memory/startup.go
@@ -29,56 +29,15 @@ func getCartridge(memory []byte) go_gb.Cartridge {
 }
 
 func getRomBanks(memory []byte) *bank {
-	banks := memory[go_gb.CartridgeROMSizeAddr]
-	var bank *bank
-	switch banks {
-	case 0x00:
-		bank = newBank(1, 32*KiB)
-	case 0x01:
-		bank = newBank(4, 16*KiB)
-	case 0x02:
-		bank = newBank(8, 16*KiB)
-	case 0x03:
-		bank = newBank(16, 16*KiB)
-	case 0x04:
-		bank = newBank(32, 16*KiB)
-	case 0x05:
-		bank = newBank(64, 16*KiB)
-	case 0x06:
-		bank = newBank(128, 16*KiB)
-	case 0x07:
-		bank = newBank(256, 16*KiB)
-	case 0x08:
-		bank = newBank(512, 16*KiB)
-	case 0x52:
-		bank = newBank(72, 16*KiB)
-	case 0x53:
-		bank = newBank(80, 16*KiB)
-	case 0x54:
-		bank = newBank(96, 16*KiB)
-	}
-	if bank == nil {
-		panic("invalid number of banks")
-	}
+	banks := go_gb.RomSize(memory[go_gb.CartridgeROMSizeAddr])
+	size, num := banks.GetSize()
+	bank := newBank(uint16(num), uint16(size))
 	bank.LoadRom(memory)
 	return bank
 }
 
 func getRamBanks(memory []byte) *bank {
-	val := memory[go_gb.CartridgeRAMSizeAddr]
-	switch val {
-	case 0x00:
-		return nil
-	case 0x01:
-		return newBank(1, 2*KiB)
-	case 0x02:
-		return newBank(1, 8*KiB)
-	case 0x03:
-		return newBank(4, 8*KiB)
-	case 0x04:
-		return newBank(16, 8*KiB)
-	case 0x05:
-		return newBank(8, 8*KiB)
-	}
-	panic("invalid RAM size")
+	val := go_gb.RamSize(memory[go_gb.CartridgeRAMSizeAddr])
+	size, num := val.GetSize()
+	return newBank(uint16(num), uint16(size))
 }
--- wasm/display.go
+++ wasm/display.go
@@ -12,20 +12,6 @@ func NewWasmDisplay() *wasmDisplay {
 	return &wasmDisplay{}
 }
 
-func (w *wasmDisplay) mapColor(col byte) (r, g, b byte) {
-	switch col {
-	case 0:
-		return 255, 255, 255
-	case 1:
-		return 0xCC, 0xCC, 0xCC
-	case 2:
-		return 0x77, 0x77, 0x77
-	case 3:
-		return 0, 0, 0
-	}
-	panic("invalid color")
-}
-
 func (w *wasmDisplay) Draw(buffer []byte) {
 	w.drawing = true
 	js.CopyBytesToJS(js.Global().Get("buffer"), buffer)
